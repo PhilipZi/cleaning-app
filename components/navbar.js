f0e371fffad2e4cd8aa187e7f10c2553876d7cb7
Philip Zimmermann <philip.zimmermann@freenet.de>
2022-05-13 15:49:59 +0200

    Add components folder and styled components

---
+++ components/navbar.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export default function Navbar(){
+    return (
+        
+    )
+}
+
+const StyledNavbar = styled.nav`
+    display: flex;
+    position: fixed;
+    justify-content: space-around;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: lightgray;
+    z-index: 0;
+`;
