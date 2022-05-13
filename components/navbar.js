f236f7f48d739428967dcf6aa378df7700d07f1d
Philip Zimmermann <philip.zimmermann@freenet.de>
2022-05-13 15:51:44 +0200

    Add navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,18 +1,16 @@
 import styled from 'styled-components';
 
-export default function Navbar(){
-    return (
-        
-    )
+export default function Navbar() {
+  return <StyledNavbar></StyledNavbar>;
 }
 
 const StyledNavbar = styled.nav`
-    display: flex;
-    position: fixed;
-    justify-content: space-around;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background-color: lightgray;
-    z-index: 0;
+  display: flex;
+  position: fixed;
+  justify-content: space-around;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: lightgray;
+  z-index: 0;
 `;
